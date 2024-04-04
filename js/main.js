13b4b1fd769310b3b751b2bd0371224392516228
irving vera <irvingvera@MacBook-Pro-de-irving.local>
2024-04-04 01:47:46 -0600

    portafolio

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const typed = new Typed('.typed',{
+    strings:[
+        '<i class="profesion">I am Programmer</i>',
+        '<i class="profesion">I am Graphic Designer</i>', 
+        '<i class="profesion">I am Informatic</i>'
+    ],
+
+    typeSpeed: 80,
+    startDelay: 300,
+    backSpeed: 80,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+
+});
+
---
+++ js/scroll.js
@@ -0,0 +1,36 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.contenedor_logo_menu', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+
+});
+
+sr.reveal('.about_me', {
+    duration: 3000,
+    origin: 'up',
+    distance: '-200px'
+
+});
+
+sr.reveal('.titulo_about_me', {
+    duration: 3000,
+    origin: 'up',
+    distance: '-200px'
+
+});
+
+sr.reveal('.titulo_portfolio', {
+    duration: 3000,
+    origin: 'right',
+    distance: '200px'
+
+});
+
+sr.reveal('.fotos', {
+    duration: 3000,
+    origin: 'right',
+    distance: '200px'
+
+});
